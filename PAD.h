a1b8c630007f38bd2bbb691bedf839d3d295ec1d
Vesa Norilo <vnorilo@gmail.com>
2013-03-21 20:31:39 +0200

    * Proper merge

--- PAD.h
+++ PAD.h
@@ -2,15 +2,11 @@
 #include <cstdint>
 
 namespace PAD{
-<<<<<<< local
 	class AudioCallbackDelegate{
 	public:
 		virtual void Process(uint64_t timestamp, const float* input, float *output) = 0;
 	};
-=======
->>>>>>> other
 
-<<<<<<< local
 	class AudioStreamConfiguration {
 		friend class AudioDevice;
 		double sampleRate;
@@ -24,37 +20,11 @@ namespace PAD{
 		void AddInputs(unsigned first = 0, unsigned last = -1);
 		void AddOutputs(unsigned first = 0, unsigned last = -1);
 		void SetPreferredBufferSize(unsigned frames);
-=======
-	typedef void (*AudioCallbackFunction)(uint64_t timestamp, const float* input, float *output, void *user);
->>>>>>> other
 
-<<<<<<< local
-		bool IsInputEnabled(unsigned index) const;
-		bool IsOutputEnabled(unsigned index) const;
-		bool IsValid() const {return valid;}
-=======
-	class AudioStreamConfiguration {
-		friend class AudioDevice;
-		double sampleRate;
-		std::vector<std::pair<unsigned,unsigned>> inputRanges;
-		std::vector<std::pair<unsigned,unsigned>> outputRanges;
-		unsigned bufferSize;
-		bool valid;
-	public:
-		AudioStreamConfiguration(double sampleRate = 44100.0);
-		void SetSampleRate(double);	
-		void AddInputs(unsigned first = 0, unsigned last = -1);
-		void AddOutputs(unsigned first = 0, unsigned last = -1);
-		void SetPreferredBufferSize(unsigned frames);
->>>>>>> other
-
-<<<<<<< local
-=======
 		bool IsInputEnabled(unsigned index) const;
 		bool IsOutputEnabled(unsigned index) const;
 		bool IsValid() const {return valid;}
 
->>>>>>> other
 		unsigned GetNumInputs();
 		unsigned GetNumOutputs();
 
@@ -80,11 +50,7 @@ namespace PAD{
 
 		virtual bool Supports(const AudioStreamConfiguration&) = 0;
 
-<<<<<<< local
 		virtual const AudioStreamConfiguration& Open(AudioCallbackDelegate&, bool startSuspended = false) = 0;
-=======
-		virtual const AudioStreamConfiguration& Open(AudioCallbackFunction, bool startSuspended = false) = 0;
->>>>>>> other
 
 		virtual void Resume() = 0;
 		virtual void Suspend() = 0;
