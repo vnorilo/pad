ae77ab92a9dfb1fc32562f0caff2ca63523c2a33
Vesa Norilo <vnorilo@siba.fi>
2013-03-21 20:27:26 +0200

    * MSVC solution

--- PAD.h
+++ PAD.h
@@ -1,42 +1,61 @@
 #include <vector>
+#include <cstdint>
 
-typedef void (*AudioCallbackFunction)(uint64_t timestamp, const float* input, float *output, void *user);
+namespace PAD{
 
-class AudioStreamConfiguration {
-friend class AudioDevice;
-	double sampleRate;
-	std::vector<std::pair<unsigned,unsigned>> inputRanges;
-	std::vector<std::pair<unsigned,unsigned>> outputRanges;
-	unsigned bufferSize;
-	bool valid;
-public:
-	AudioStreamConfiguration(double sampleRate = 44100.0);
-	void SetSampleRate(double);	
-	void AddInputs(unsigned first = 0, unsigned last = -1);
-	void AddOutputs(unsigned first = 0, unsigned last = -1);
-	void SetPreferredBufferSize(unsigned frames);
+	typedef void (*AudioCallbackFunction)(uint64_t timestamp, const float* input, float *output, void *user);
 
-	bool IsInputEnabled(unsigned index) const;
-	bool IsOutputEnabled(unsigned index) const;
-	bool IsValid() const {return valid;}
+	class AudioStreamConfiguration {
+		friend class AudioDevice;
+		double sampleRate;
+		std::vector<std::pair<unsigned,unsigned>> inputRanges;
+		std::vector<std::pair<unsigned,unsigned>> outputRanges;
+		unsigned bufferSize;
+		bool valid;
+	public:
+		AudioStreamConfiguration(double sampleRate = 44100.0);
+		void SetSampleRate(double);	
+		void AddInputs(unsigned first = 0, unsigned last = -1);
+		void AddOutputs(unsigned first = 0, unsigned last = -1);
+		void SetPreferredBufferSize(unsigned frames);
 
-	unsigned GetBufferSize() const {return bufferSize;}
-	double GetSampleRate() const {return sampleRate;}
+		bool IsInputEnabled(unsigned index) const;
+		bool IsOutputEnabled(unsigned index) const;
+		bool IsValid() const {return valid;}
+
+		unsigned GetNumInputs();
+		unsigned GetNumOutputs();
+
+		unsigned GetBufferSize() const {return bufferSize;}
+		double GetSampleRate() const {return sampleRate;}	
+	};
+
+	class AudioDevice;
+	class AudioDeviceCollection {
+		AudioDevice *b, *e;
+	public:
+		AudioDeviceCollection(AudioDevice *b, AudioDevice *e):b(b),e(e){}
+		AudioDevice* begin() {return b;}
+		AudioDevice* end() {return e;}
+	};
+
+	class AudioDevice {	
+	public:
+		virtual unsigned GetNumInputs() = 0;
+		virtual unsigned GetNumOutputs() = 0;
+		virtual const char *GetName() = 0;
+		virtual const char *GetHostAPI() = 0;
+
+		virtual bool Supports(const AudioStreamConfiguration&) = 0;
+
+		virtual const AudioStreamConfiguration& Open(AudioCallbackFunction, bool startSuspended = false) = 0;
+
+		virtual void Resume() = 0;
+		virtual void Suspend() = 0;
+
+		virtual void Close() = 0;
+
+		static AudioDeviceCollection Enumerate();
+	};
 };
 
-class AudioDevice {	
-public:
-	virtual unsigned GetNumInputs() = 0;
-	virtual unsigned GetNumOutputs() = 0;
-	virtual const char *GetName() = 0;
-	virtual const char *GetHostAPI() = 0;
-	
-	virtual bool Supports(const AudioStreamConfiguration&) = 0;
-
-	virtual const AudioStreamConfiguration& Open(AudioCallbackFunction, bool startSuspended = false);
-	
-	virtual void Resume();
-	virtual void Suspend();
-
-	virtual void Close();
-};
