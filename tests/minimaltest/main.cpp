34a65729be3d1737a07485e3ce876b400ea13260
xenakios <xenakios@localhost>
2013-03-21 20:47:09 +0200

    Added function to get version string. Change minimal test program to test.

--- tests/minimaltest/main.cpp
+++ tests/minimaltest/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
+#include "pad.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello from PAD!" << endl;
-    cout << "..." << endl;
-    cout << "pruits!" << endl;
+    cout << "Hello from PAD "<<PAD::versionString()<<"!"<<endl;
     return 0;
 }
 
