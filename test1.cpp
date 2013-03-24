ac6135bc6856105e4e34385ae4b09dea983db0a5
Vesa Norilo <vnorilo@gmail.com>
2013-03-24 19:43:36 +0200

    * ASIO input * Vectored channel interleavers don't crash on misaligned buffers any more 	- Fallback to misaligned loads, hoisted out of loops by templates * Named constructor idiom for stream configuration

--- test1.cpp
+++ test1.cpp
@@ -7,29 +7,49 @@
 int main()
 {
 	using namespace PAD;
-	Session myAudioSession;
+	
+	class ErrorLogger : public DeviceErrorDelegate {
+	public:
+		void Catch(SoftError e) {std::cerr << "*Soft "<<e.GetCode()<<"* :" << e.what() << "\n";}
+		void Catch(HardError e) {std::cerr << "*Hard "<<e.GetCode()<<"* :" << e.what() << "\n";}
+	};
+	
+	Session myAudioSession(true,&ErrorLogger());
+
+
 	for(auto& dev : myAudioSession)
 	{
 		std::cout << dev << "\n  * Stereo : " << dev.DefaultStereo() 
 						 << "\n  * All    : " << dev.DefaultAllChannels() << "\n\n";
 	}
 
-	auto rme(myAudioSession("ASIO Fireface"));
-	if (rme)
+	auto asioDevice = myAudioSession.FindDevice("Fireface");
+
+	if (asioDevice != myAudioSession.end())
 	{
 		double phase = 0;
-		auto audioProcess = Closure(([&](uint64_t time, const PAD::AudioStreamConfiguration&, const float *input, float *output, unsigned frames)
+		auto myAudioProcess = Closure(([&](uint64_t time, const PAD::AudioStreamConfiguration& cfg, const float *input, float *output, unsigned frames)
 		{
+			unsigned numOuts(cfg.GetNumStreamOutputs());
+			unsigned numIns(cfg.GetNumStreamInputs());
 			for(unsigned i(0);i<frames;++i)
 			{
-				output[i] = sin(phase);
-				phase = phase + 0.01 * M_PI;
+				for(unsigned j(0);j<numOuts;++j)
+				{
+					output[i*numOuts+j] = (float)sin(phase * (1.0 + double(j)/numOuts));
+					if (j<numIns) output[i*numOuts+j] = input[i*numIns+j];
+					phase = phase + 0.01 * M_PI;
+				}
 			}
 		}));
 
-		rme->Open(SampleRate(44100) + Outputs(0,4),audioProcess);
+		std::cout << "Actual stream parameters: " <<
+		asioDevice->Open(Stream().SampleRate(96000)
+								 .StereoInput(0)
+								 .StereoOutput(1)
+								 .Delegate(myAudioProcess));
 		getchar();
-		rme->Close();
+		asioDevice->Close();
 	}
 
 }
