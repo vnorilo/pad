c794e001ab05572082b691bc158133d682e63826
vnorilo <vnorilo@localhost>
2014-12-11 13:51:08 +0200

    * Template fix

--- pad_samples_sse2.cpp
+++ pad_samples_sse2.cpp
@@ -4,17 +4,5 @@
 
 namespace PAD{
 	namespace Converter{
-		template <> void Transpose<>(SampleVector<float,4> *v)
-		{
-			__m128i t0 = _mm_castps_si128(_mm_unpacklo_ps(v[0].data,v[1].data));
-			__m128i t1 = _mm_castps_si128(_mm_unpacklo_ps(v[2].data,v[3].data));
-			__m128i t2 = _mm_castps_si128(_mm_unpackhi_ps(v[0].data,v[1].data));
-			__m128i t3 = _mm_castps_si128(_mm_unpackhi_ps(v[2].data,v[3].data));
-
-			v[0] = _mm_castsi128_ps(_mm_unpacklo_epi64(t0,t1));
-			v[1] = _mm_castsi128_ps(_mm_unpackhi_epi64(t0,t1));
-			v[2] = _mm_castsi128_ps(_mm_unpacklo_epi64(t2,t3));
-			v[3] = _mm_castsi128_ps(_mm_unpackhi_epi64(t2,t3));
-		}
 	}
 }
